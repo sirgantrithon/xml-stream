fd4bbfe22d70810e396a9b2847e59b3ec7501bd0
Ian Andrews <sirgantrithon@gmail.com>
2014-06-09 20:47:55 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+-----------------
+# XML Stream Test
+-----------------
+
+This project is intended to test using a streaming XML parser with the Vertx
+async HTTP client.
---
+++ src/main/java/com/sirgantrithon/xml/stream/XMLStreamServer.java
@@ -0,0 +1,10 @@
+package com.sirgantrithon.xml.stream;
+
+import org.vertx.java.platform.Verticle;
+
+public class XMLStreamServer extends Verticle {
+
+	public void start() {
+
+	}
+}
